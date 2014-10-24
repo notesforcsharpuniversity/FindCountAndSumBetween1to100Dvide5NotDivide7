b574b1d733cd461216de085ba54fc9e21fb845e1
Ali Osman Mollahüseyinoğlu <aliosman.m@hotmail.com>
2014-10-24 16:45:18 +0300

    FindCountAndSumBetween1to100Dvide5NotDivide7
    Find the count and sum of the numbers between 1 to 100 which can divide
    to 5 but cannot divided to 7

---
+++ FindToBetween1to100WhichDivide5Not7/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FindToBetween1to100WhichDivide5Not7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int sum = 0;
+            int  count = 1;
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i%5==0 && i%7!=0)
+                {
+                    sum += i;
+                    count++;
+                }
+            }
+            MessageBox.Show("Total sum:" + sum.ToString() + "\nCount : " + count.ToString());
+        }
+    }
+}
---
+++ FindToBetween1to100WhichDivide5Not7/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FindToBetween1to100WhichDivide5Not7")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("FindToBetween1to100WhichDivide5Not7")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d452ca2c-6823-4380-9313-7a64ecb2a425")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
